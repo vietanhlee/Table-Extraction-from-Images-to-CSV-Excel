1f8a368e15297a8c8ddad46810cb66db204a0a3d
vietanhlee <levietanhtrump@gmail.com>
2025-04-16 16:56:50 +0700

    up file

--- predict.py
+++ predict.py
@@ -1,10 +1,9 @@
 from Processing import Processing
 import pandas as pd
 
-path = r'G:\Table Extraction from Images to CSV Excel\image test\7.jpg'
+path = r'image test\7.jpg'
 
 tool = Processing(gpu= False)
 DF = tool.process_single_image(path, draw= 0)
 print(pd.DataFrame(DF))
-
 

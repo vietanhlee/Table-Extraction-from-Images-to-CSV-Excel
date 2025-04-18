8b46ff39dafdb401a73b90440f253f406d650517
vietanhlee <levietanhtrump@gmail.com>
2025-04-18 10:40:43 +0700

    push

--- Processing.py
+++ Processing.py
@@ -6,11 +6,10 @@ import matplotlib.pyplot as plt
 import pandas as pd
 from concurrent.futures import ThreadPoolExecutor, as_completed
 import os
-
 class Processing:
     def __init__(self, num_threads=4, gpu = False):
         # For task recogineze
-        self.reader = easyocr.Reader(['vi'], verbose=False, gpu= gpu)
+        self.reader = easyocr.Reader(['vi', 'en'], verbose=False, gpu= gpu)
         # For task detect
         self.paddle_reader = PaddleOCR(lang='en', show_log=False,use_gpu = gpu)
         # For run many task simultaneously (don't test this function, it is not complete) 
@@ -22,18 +21,20 @@ class Processing:
         if image is None:
             raise FileNotFoundError(f"Cannot read image at path: {img_path}")
         img = image.copy()
-
+        a = 10
+        b = a / 1.5
         h, w = image.shape[:2]
-        image_resized = cv2.resize(image, (int(w * 2.4), int(h * 1.2)))
-        result = self.paddle_reader.ocr(image_resized, cls=True)[0]
+        image_resized = cv2.resize(image, (int(w * a), int(h * b)))
+        result = self.paddle_reader.ocr(image_resized, cls=True, rec= False)[0][::-1]
 
         rects, horizontal_list, texts = [], [], []
 
+        padding = 2
         for line in result:
-            box = np.array(line[0], dtype=np.float32)
+            box = np.array(line, dtype=np.float32)
             x, y, w, h = cv2.boundingRect(box)
-            x1, y1 = int(x / 2.4), int(y / 1.2)
-            x2, y2 = int((x + w) / 2.4), int((y + h) / 1.2)
+            x1, y1 = max(int(int(x / a) - padding), 0), max(int(int(y / b) - padding), 0)
+            x2, y2 = int(int((x + w) / a) + padding), int(int((y + h) / b) + padding)
 
             rects.append([x1, y1, x2, y2])
             horizontal_list.append([x1, x2, y1, y2])
@@ -45,6 +46,8 @@ class Processing:
             text = text.strip()
             if text == '1.': text = 'I.'
             elif text == '11.': text = 'II.'
+            elif len(text) >= 2 and text[0] == '[' and text[-1].isdigit():
+                text = '1'+ text[1:]
             else:
                 chars = list(text)
                 for i in range(1, len(chars)):
@@ -62,7 +65,57 @@ class Processing:
             plt.show()
 
         return rects, texts
+    def find_rects_texts_paddle_all(self, img_path, mode_draw= 0):
+        '''Function for find rects (bounding box) and text respectively'''
+        image = cv2.imread(img_path)
+        if image is None:
+            raise FileNotFoundError(f"Cannot read image at path: {img_path}")
+        img = image.copy()
+        a = 10
+        b = a / 1.5
+        h, w = image.shape[:2]
+        image_resized = cv2.resize(image, (int(w * a), int(h * b)))
+        result = self.paddle_reader.ocr(image_resized, cls=True, rec= True)[0]
 
+        rects, horizontal_list, texts = [], [], []
+
+        padding = 2
+        for line in result:
+            box = np.array(line[0], dtype=np.float32)
+            
+            x, y, w, h = cv2.boundingRect(box)
+            
+            x1, y1 = max(int(int(x / a) - padding), 0), max(int(int(y / b) - padding), 0)
+            x2, y2 = int(int((x + w) / a) + padding), int(int((y + h) / b) + padding)
+
+            rects.append([x1, y1, x2, y2])
+            
+            text = line[1][0]
+            text = text.strip()
+            if text == '1.': text = 'I.'
+            elif text == '11.': text = 'II.'
+            elif len(text) >= 2 and text[0] == '[' and text[-1].isdigit():
+                text = '1'+ text[1:]
+            else:
+                chars = list(text)
+                for i in range(1, len(chars)):
+                    if chars[i] == '1' and chars[i - 1] == 'I':
+                        chars[i] = 'I'
+                text = ''.join(chars)
+            texts.append(text)
+            
+        rects = np.array(rects, dtype=int)
+
+        if mode_draw:
+            for rect in rects:
+                cv2.rectangle(img, (rect[0], rect[1]), (rect[2], rect[3]), (0, 255, 0), 1)
+            plt.figure(figsize=(12, 12))
+            plt.imshow(cv2.cvtColor(img, cv2.COLOR_BGR2RGB))
+            plt.axis('off')
+            plt.show()
+
+        return rects, texts
+        
     def check_line(self, box1, box2):
         '''Check two bounding are all on a line'''
         return abs((box1[1] + box1[3]) // 2 - (box2[1] + box2[3]) // 2) < 10
@@ -124,6 +177,11 @@ class Processing:
 
     def process_single_image(self, img_path, draw=0):
         rects, texts = self.find_rects_texts(img_path, draw)
+        rects_grouped, texts_grouped, n_cols = self.rects_texts_ncollum_processed(rects, texts)
+        box_cols = self.find_box_cols(rects_grouped, n_cols)
+        return self.find_text_each_row(box_cols, rects_grouped, texts_grouped)
+    def process_single_image_paddle_all(self, img_path, draw=0):
+        rects, texts = self.find_rects_texts_paddle_all(img_path, draw)
         rects_grouped, texts_grouped, n_cols = self.rects_texts_ncollum_processed(rects, texts)
         box_cols = self.find_box_cols(rects_grouped, n_cols)
         return self.find_text_each_row(box_cols, rects_grouped, texts_grouped)
